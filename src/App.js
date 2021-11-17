0327ed70f142b948cc701560db9ee16ae9a9ca11
Ze <louisliu000123@gmail.com>
2021-11-16 21:29:09 -0330

    Initial the project in order to do my own.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,4 @@
-const App = (props) => {
-  const { notes } = props;
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+const App = () => {
+  
 
 export default App;
