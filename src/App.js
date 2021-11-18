2e583933a3299825718b7d8a0c8012ff08bd248c
Ze <louisliu000123@gmail.com>
2021-11-18 16:08:54 -0330

    Add insert a note functionalities. Add a form to do that.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import NoteContent from "./components/NoteContent";
+import NoteContent from './components/NoteContent';
+import { useState } from 'react';
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note...');
+
+  const onChangeHandler = (event) => {
+    event.preventDefault();
+    console.log("Event", event);
+    console.log('Event.target', event.target);
+    console.log('Event.target.value', event.target.value);
+    setNewNote(event.target.value);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const notesObject = {
+      id: notes.length +1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+    };
+    setNotes(notes.concat(notesObject));
+    //setNotes(notes.concat(newNote));
+  }
+
   return (
-    <>
+    <div>
       <h1>Notes</h1>
       <ul>
         {notes.map((note) => (
-          <NoteContent note={note}></NoteContent>
+          <NoteContent key={note.id} note={note} />
         ))}
       </ul>
-    </>
+      <form onSubmit={submitHandler}>
+        <input value={newNote} onChange={onChangeHandler}/>
+        <button type='submit'>Insert your note.</button>
+      </form>
+    </div>
   );
 };
+
 export default App;
